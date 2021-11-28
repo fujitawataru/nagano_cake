3a680ff7c41738120e2e605be7b1ae94320f651c
EC2 Default User <ec2-user@ip-172-31-36-10.ec2.internal>
2021-11-28 10:08:15 +0000

    [Add]新規投稿、一覧画面

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,32 @@
+class Admin::ItemsController < ApplicationController
+
+
+def new
+  @item = Item.new
+end
+
+def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+end
+
+def index
+ @items = Item.all
+end
+
+def show
+end
+
+def edit
+end
+  
+def update
+end
+
+private
+
+def item_params
+    params.require(:item).permit(:genre,:name,:image,:introduction,:price,:is_active)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :admins, :controllers => {
+    :registrations => 'admins/rejistrations',
+    :sessions => 'admins/sessions'
+  }
+  devise_scope :admins do
+    get "sign_in", :to => "admins/sessions#new"
+    get "sign_out", :to => "admins/sessions#destoroy"
+  end
+  
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 root 'homes#top'
 get 'home/about' => 'homes#about'
+
+namespace :admin do
+  resources :items, only: [ :index, :new, :create, :show, :edit, :update]
+end
+
 end
